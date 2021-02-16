01f33de1201fc0c91349227ae6f275474f6dce97
makemanback <makemanback@gmail.com>
2021-02-16 22:34:12 +0300

    добавляет переход по карточкам фильмов

--- src/components/small-card/small-card.jsx
+++ src/components/small-card/small-card.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
 import Video from "../video/video";
 
-const SmallCard = ({image, title}) => {
+const SmallCard = ({id, image, title}) => {
 
   const [isVideo, setVideo] = useState(false);
   let timeOutId = null;
@@ -40,7 +40,7 @@ const SmallCard = ({image, title}) => {
         {isVideo ? <Video title={title} /> : <img src={image} alt={title} width="280" height="175" />}
       </div>
       <h3 className="small-movie-card__title">
-        <Link onClick={() => {}} className="small-movie-card__link" to="films/:id">{title}</Link>
+        <Link onClick={() => {}} className="small-movie-card__link" to={`films/${id}`}>{title}</Link>
       </h3>
     </article>
   );
@@ -48,7 +48,8 @@ const SmallCard = ({image, title}) => {
 
 SmallCard.propTypes = {
   title: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired
+  image: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired
 };
 
 export default SmallCard;
